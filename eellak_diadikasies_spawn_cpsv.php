e4e09845b630b9085e18f097a20379c285b11c65
David Bromoiras <bromoiras.david@gmail.com>
2017-03-28 14:24:57 +0300

    create the outline for the handler function for spawning cpsv class pages

---
+++ eellak_diadikasies_spawn_cpsv.php
@@ -0,0 +1,8 @@
+<?php
+
+class Executor{
+  public static function onPageContentSaveComplete( $article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId ) {    
+  
+    
+  }
+}
