949b37cf802389a0c32f7fb90fc6940141964d73
David Bromoiras <bromoiras.david@gmail.com>
2017-09-04 10:35:55 +0300

    access saved article content, attempt to modify, still buggy infinite instances save

--- cpsv.php
+++ cpsv.php
@@ -40,33 +40,62 @@ class CPSVSpawn{
 	
   public static function onPageContentSaveComplete($article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId) {    
   
-		/**
-		 * define the content of a test page to build... currently too buggy, but works.
-		 */
-		$article_title=Title::newFromText('dave'.rand(0, 100000));
-		
-		/**
-		 * ContentHandler has static methods for creating the content for a mw page.
-		 */
-		$article_content=ContentHandler::makeContent('test content', $article_title);
-		
-		/**
-		 * WikiPage has static factory methods for creating new WikiPages for any
-		 * namespace and of any type
-		 */
-		$public_service_page=WikiPage::factory($article_title);
-		//$test_user=User::newFromId(2);
-		
-		/**
-		 * CURRENTLY WORKING ON THIS in order to see if there is a certain structured
-		 * representation for the wikiPage or that i should proceed with parsing the
-		 * content for keywords.
-		 */
+    $service_template_map=array();
 		$content_text=ContentHandler::getContentText($content);
-		if($user){
-			$public_service_page->doEditContent($article_content, $content_text, $flags, $baseRevId, $user);
-			wfErrorLog($content_text, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
-		}
-		return true;
+  
+    $service_description_value;
+    
+    /**
+     * Cut out the template to parse its contents line by line
+     */
+    $tmpl_start=mb_strpos($content_text, '{{Καταχωρημένη Υπηρεσία', 0, 'UTF-8');
+    $tmpl_end=mb_strpos($content_text, '}}');
+    
+    if(!is_null($tmpl_start)){
+      $service_template_map=preg_split("/\\r\\n|\\r|\\n/", $content_text);
+      if (sizeof($service_template_map)){
+        //wfErrorLog('*****size of map'.sizeof($service_template_map), '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+      }
+      
+      foreach($service_template_map as $map_entry){
+        
+        $i=0;
+        /**
+         * Check that the template field has indeed been 
+         */
+        if(mb_stristr($map_entry, "input_service_description", false, 'UTF-8')){
+          $tmpl_value=mb_substr($map_entry, mb_strpos($map_entry, '=', NULL, 'UTF-8')+1);
+          $service_description_value="== Περιγραφη Υπηρεσιας == ".$i.PHP_EOL.$tmpl_value.PHP_EOL;
+          //wfErrorLog("Περιγραφη Υπηρεσιας => ".$service_description_value, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+        }
+      }
+      
+      /**
+       * define the content of a test page to build... currently too buggy, but works.
+       */
+      $article_title=Title::newFromText('dave'.rand(0, 100000));
+
+      /**
+       * ContentHandler has static methods for creating the content for a mw page.
+       */
+      $article_content=ContentHandler::makeContent('test content', $article_title);
+
+      /**
+       * WikiPage has static factory methods for creating new WikiPages for any
+       * namespace and of any type
+       */
+      //$public_service_page=WikiPage::factory($article_title);
+      //$test_user=User::newFromId(2);
+
+  		$content_text=$content_text.$service_description_value;
+      if($user){
+//        $public_service_page->doEditContent($article_content, $content_text, $flags, $baseRevId, $user);
+        $artcl_status=$article->doEditContent(ContentHandler::makeContent($content_text, $article->getTitle()), 'test for template rewrite', EDIT_UPDATE, $baseRevId, $user);
+        //wfErrorLog($artcl_status, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+      }
+    }
+    //wfErrorLog($content_text, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+		
+    return true;
   }
 }
