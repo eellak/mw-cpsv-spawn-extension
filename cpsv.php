b90972caebf7780d5e5e967d78a69d053ad1298e
David Bromoiras <bromoiras.david@gmail.com>
2017-09-04 15:14:10 +0300

    deal with the infinite loop of the pagesave hook

--- cpsv.php
+++ cpsv.php
@@ -33,13 +33,20 @@ $wgExtensionCredits['other'][] = array(
 	 * 
 	 * https://www.mediawiki.org/wiki/Manual:Hooks/PageContentSaveComplete
 	 */
+//	$wgHooks["PageContentSaveComplete"][] = "CPSVSpawn::onPageContentSaveComplete";
 	$wgHooks["PageContentSaveComplete"][] = "CPSVSpawn::onPageContentSaveComplete";
 
 class CPSVSpawn{
 	
 	
+//  public static function onPageContentSave($article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId) {    
+	private static $g_article_id=-1;
+//	private $g_user;
+//	private $g_content;
+	
   public static function onPageContentSaveComplete($article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId) {    
   
+		
     $service_template_map=array();
 		$content_text=ContentHandler::getContentText($content);
   
@@ -51,10 +58,11 @@ class CPSVSpawn{
     $tmpl_start=mb_strpos($content_text, '{{Καταχωρημένη Υπηρεσία', 0, 'UTF-8');
     $tmpl_end=mb_strpos($content_text, '}}');
     
-    if(!is_null($tmpl_start)){
+    if(!is_null($tmpl_start) && (self::$g_article_id===-1 || self::$g_article_id!=$article->getId()) && $flags===1){ //	EDIT_NEW
+			self::$g_article_id=$article->getId();
       $service_template_map=preg_split("/\\r\\n|\\r|\\n/", $content_text);
       if (sizeof($service_template_map)){
-        //wfErrorLog('*****size of map'.sizeof($service_template_map), '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+        wfErrorLog('*****size of map'.sizeof($service_template_map), '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
       }
       
       foreach($service_template_map as $map_entry){
@@ -65,8 +73,8 @@ class CPSVSpawn{
          */
         if(mb_stristr($map_entry, "input_service_description", false, 'UTF-8')){
           $tmpl_value=mb_substr($map_entry, mb_strpos($map_entry, '=', NULL, 'UTF-8')+1);
-          $service_description_value="== Περιγραφη Υπηρεσιας == ".$i.PHP_EOL.$tmpl_value.PHP_EOL;
-          //wfErrorLog("Περιγραφη Υπηρεσιας => ".$service_description_value, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+          $service_description_value="== Περιγραφη Υπηρεσιας == ".PHP_EOL.$tmpl_value.PHP_EOL;
+          wfErrorLog("Περιγραφη Υπηρεσιας => ".$service_description_value, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
         }
       }
       
@@ -90,11 +98,16 @@ class CPSVSpawn{
   		$content_text=$content_text.$service_description_value;
       if($user){
 //        $public_service_page->doEditContent($article_content, $content_text, $flags, $baseRevId, $user);
-        $artcl_status=$article->doEditContent(ContentHandler::makeContent($content_text, $article->getTitle()), 'test for template rewrite', EDIT_UPDATE, $baseRevId, $user);
-        //wfErrorLog($artcl_status, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+//				ContentHandler::makeContent($content_text, $article->getTitle());
+				$new_content=new WikitextContent($content_text);
+//        $artcl_status=$article->doEditContent(ContentHandler::makeContent($content_text, $article->getTitle()), 'test for template rewrite', EDIT_UPDATE, $baseRevId, $user);
+        $artcl_status=$article->doEditContent($new_content, 'test for template rewrite', 2); // 2 stands for EDIT_UPDATE. ref:	https://doc.wikimedia.org/mediawiki-core/1.27.1/php/group__Constants.html
+        wfErrorLog($artcl_status, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+        wfErrorLog("the page id: ".$article->getId().PHP_EOL, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+        wfErrorLog($article->getTitle(), '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
       }
     }
-    //wfErrorLog($content_text, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
+    wfErrorLog($content_text, '/var/www/sftp_webadmins/sites/dev-wiki.ellak.gr/public/log/file_debug.log');
 		
     return true;
   }
